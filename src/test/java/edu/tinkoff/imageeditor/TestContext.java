b3e56215c40da111b488a6a62725670f85609e27
Ivanuil <ivo-pronin@yandex.ru>
2024-03-29 01:01:08 +0300

    Switched to TestContainers

--- src/test/java/edu/tinkoff/imageeditor/TestContext.java
+++ src/test/java/edu/tinkoff/imageeditor/TestContext.java
@@ -1,7 +1,14 @@
 package edu.tinkoff.imageeditor;
 
+import edu.tinkoff.imageeditor.config.MinIOTestConfig;
+import edu.tinkoff.imageeditor.config.PostgreSQLTestConfig;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(initializers = {
+        MinIOTestConfig.Initializer.class,
+        PostgreSQLTestConfig.Initializer.class
+})
 public class TestContext {
 }
